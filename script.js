4b3c7758b739ac575dd54b06decf9c55d20a9362
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-11 03:42:14 +0500

    Mobile hamburger toggle done

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get the menu toggle button
+const menuToggleButton = document.getElementById("menu-toggle");
+
+// Get the mobile menu div
+const mobileMenu = document.getElementById("mobile-menu");
+
+// Add a click event listener to the menu toggle button
+menuToggleButton.addEventListener("click", function () {
+	// Toggle the hidden class on the mobile menu
+	console.log("clik-toggle-hidden");
+	mobileMenu.classList.toggle("hidden");
+});
