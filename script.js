1707c3c1537d218dc4f09b5bb406b940d35eb551
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-11 19:43:43 +0500

    Javascript file cleaned

--- script.js
+++ script.js
@@ -7,6 +7,5 @@ const mobileMenu = document.getElementById("mobile-menu");
 // Add a click event listener to the menu toggle button
 menuToggleButton.addEventListener("click", function () {
 	// Toggle the hidden class on the mobile menu
-	console.log("clik-toggle-hidden");
 	mobileMenu.classList.toggle("hidden");
 });
